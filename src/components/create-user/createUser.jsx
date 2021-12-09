10cb2eb6d304e5ee7ab305dae727d29a51b62e0e
w84april <donyaschefer@gmail.com>
2021-12-09 19:57:16 +0300

    Add change account handler

--- src/components/create-user/createUser.jsx
+++ src/components/create-user/createUser.jsx
@@ -8,33 +8,35 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Autocomplete from "@mui/material/Autocomplete";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import { useWeb3 } from "../../hooks/useWeb3";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
-const roles = [
-  { label: "Admin", value: 2 },
-  { label: "Engineer", value: 1 },
-  { label: "Worker", value: 0 },
-];
+import { useState } from "react";
 
 const theme = createTheme();
 
 export default function CreateUser() {
-  const { accounts, contract } = useWeb3();
+  const { account, contract } = useWeb3();
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [role, setRole] = useState(0);
-  console.log(accounts, contract);
+  const [open, setOpen] = useState(false);
+  console.log(account, contract);
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(name, 123432, address, role, accounts[0]);
+    console.log(account);
     contract.methods
-      .createUser(name, 123234, address, role)
-      .send({ from: accounts[0], gas: 4700000, gasPrice: 8000000000 })
+      .createUser(name, +new Date(), address, role)
+      .send({ from: account, gas: 4700000, gasPrice: 8000000000 })
       .then((res) => console.log(res))
-      .catch(console.log);
+      .catch(() => setOpen(true));
   };
 
   return (
@@ -86,20 +88,22 @@ export default function CreateUser() {
                 />
               </Grid>
               <Grid item xs={12}>
-                <Autocomplete
-                  disablePortal
-                  id="combo-box-demo"
-                  options={roles}
-                  sx={{ width: 500 }}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Movie" />
-                  )}
-                  value={role}
-                  onChange={(e, role) => setRole(role.value)}
-                  isOptionEqualToValue={(option, value) =>
-                    option.value === value.value
-                  }
-                />
+                <Box sx={{ minWidth: 120 }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Role</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={role}
+                      label="Age"
+                      onChange={(e) => setRole(e.target.value)}
+                    >
+                      <MenuItem value={0}>Worker</MenuItem>
+                      <MenuItem value={1}>Engineer</MenuItem>
+                      <MenuItem value={2}>Admin</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
               </Grid>
             </Grid>
             <Button
@@ -112,6 +116,25 @@ export default function CreateUser() {
             </Button>
           </Box>
         </Box>
+        {open && (
+          <Alert
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+            severity="error"
+          >
+            Something went wrong
+          </Alert>
+        )}
       </Container>
     </ThemeProvider>
   );
--- src/hooks/useWeb3.js
+++ src/hooks/useWeb3.js
@@ -4,20 +4,19 @@ import Web3 from "web3";
 import { ABI, ADDRESS } from "./../config";
 
 export const useWeb3 = () => {
-  const [accounts, setAccounts] = useState("");
+  const [account, setAccount] = useState("");
   const [contract, setContract] = useState({});
+
+  const handleChangeAccount = (account) => {
+    setAccount(account[0]);
+  };
+  window.ethereum.on("accountsChanged", handleChangeAccount);
+
   const getData = useCallback(async () => {
     await window.ethereum.enable();
     const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
     const accounts = await web3.eth.getAccounts();
-    console.log("not:", accounts);
-    const checksummedAccounts = accounts.map((account) =>
-      web3.utils.toChecksumAddress(account)
-    );
-    console.log("yes:", checksummedAccounts);
-    web3.defaultAccount = checksummedAccounts[0];
-
-    setAccounts(checksummedAccounts);
+    setAccount(accounts[0]);
     const contract = new web3.eth.Contract(ABI, ADDRESS);
     setContract(contract);
   }, []);
@@ -25,5 +24,5 @@ export const useWeb3 = () => {
     getData();
   }, [getData]);
 
-  return { accounts, contract };
+  return { account, contract };
 };
